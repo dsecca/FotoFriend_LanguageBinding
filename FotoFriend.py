ab34fc40b4647f6a093e9b7a9bdd439c55aaf896
dsecca <davsecc1995@gmail.com>
2017-11-25 16:09:00 -0500

    Basic upload image feature works. Missing file error checking.

--- FotoFriend.py
+++ FotoFriend.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import httplib2
+import os
 
 #Python FotoFriend API
 
@@ -8,6 +9,7 @@ class FotoFriend():
     def __init__(self):
         #Credentials
         self.http_server = "fotofriendserver.us-west-2.elasticbeanstalk.com"
+        self.ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png'])
 
     def login(self, username):
         #Connect to FotoFriend server
@@ -17,24 +19,11 @@ class FotoFriend():
 
         return response.json()
 
-    def uploadImage(self, fileObject):
+    def uploadImage(self, fileObject, uploadFolderPath, sessionUsername):
+        
+        response = requests.post("http://%s/storeImage" % self.http_server, files={'file': open(os.path.join(uploadFolderPath, fileObject.filename), 'rb'), 'username': sessionUsername})
 
-        #Add the picture to the path where pictures will be stored
-        if checkFileExtension(fileObject.name):
-            #Send file to Backend Server
-            file.save(os.path.join(application.config['UPLOAD_FOLDER'], fileObject.name))
-
-            response = requests.post("http://%s/storeImage" % self.http_server, files={'file': open(os.path.join(application.config['UPLOAD_FOLDER'], file.filename), 'rb'), 'username': flask.session['username']})
-
-            os.remove(os.path.join(application.config['UPLOAD_FOLDER'], file.filename))
-
-            if response.status_code == 200:
-                flask.flash("Your upload was successful!")
-
-            return flask.redirect(flask.url_for('home'))
-        else:
-            flask.flash("Only jpeg, jpg and png files are supported. Please try again.")
-            return flask.redirect(flask.url_for('home'))
+        return response
 
     #Check whether the filename extension is allowed 
     def checkFileExtension(filename):
