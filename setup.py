b18b077606dbd818613a6cff0e814fa99b54ce90
t-vaudry <tvaudryread@gmail.com>
2017-11-26 22:11:26 -0500

    Cleaning up travis deployment

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@
 
 See:
 https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
+import os
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+build_number = None
+
+if 'TRAVIS_BUILD_NUMBER' in os.environ:
+    build_number = os.environ['TRAVIS_BUILD_NUMBER']
+else
+    build_number = 'DEV'
 
 setup(
     name='fotofriend',
@@ -23,7 +29,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.%s' % build_number,
 
     description='Python Binding for FotoFriend',
     long_description=long_description,
